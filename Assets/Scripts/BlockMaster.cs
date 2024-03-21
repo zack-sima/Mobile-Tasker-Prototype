95a7b183fae37db115659138472b172c154cdbcc
Zack Sima <simayance@gmail.com>
2024-03-20 19:42:28 -0700

    base

---
+++ Assets/Scripts/BlockMaster.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Controls the dragging & arrangement of blocks, and the scroll view on the Main Canvas.
+/// </summary>
+
+public class BlockMaster : MonoBehaviour {
+	public static BlockMaster instance;
+
+	#region Prefabs
+
+	[SerializeField] private GameObject blockPrefab;
+
+	#endregion
+
+	#region Constants
+
+	public const float BLOCK_SPACING = 20f; //spacing between blocks
+	public const float BLOCK_TOP_PADDING = 50f; //extra padding at top
+
+	#endregion
+
+	#region References & Getters
+
+	[SerializeField] private Canvas mainCanvas;
+	public Canvas GetMainCanvas() { return mainCanvas; }
+	public float GetCanvasScaleFactor() { return mainCanvas.scaleFactor; }
+
+	[SerializeField] private ScrollRect scrollViewParent;
+	[SerializeField] private RectTransform scrollViewContent;
+
+	//shows where the block would go with indents when a block is dragged
+	[SerializeField] private RectTransform blockShadow;
+
+	#endregion
+
+	#region Members
+
+	//keep track of all the parent blocks (each block has a list of its children)
+	private List<TaskBlock> blocks = new();
+	public List<TaskBlock> GetBlocks() { return blocks; }
+
+	#endregion
+
+	//called by button; creates empty block in FRONT
+	public void CreateBlock() {
+		GameObject newBlockObj = Instantiate(blockPrefab, scrollViewContent);
+
+		//TODO: temp id set
+		newBlockObj.GetComponent<TaskBlock>().SetBlockID(blocks.Count);
+
+		blocks.Insert(0, newBlockObj.GetComponent<TaskBlock>());
+		RecalculateBlocks();
+	}
+	public void SetShadow(Vector2 sizeDelta, float heightOffset, float leftOffset) {
+		if (!blockShadow.gameObject.activeInHierarchy)
+			blockShadow.gameObject.SetActive(true);
+
+		blockShadow.anchoredPosition = new Vector2(0, heightOffset);
+		blockShadow.sizeDelta = sizeDelta;
+
+		//left offset
+		blockShadow.offsetMin = new Vector2(leftOffset, blockShadow.offsetMin.y);
+	}
+	public void RecalculateBlocks() {
+		//re-index all blocks' heights
+		float totalContentHeight = TaskBlock.DEFAULT_HEIGHT / 2f - BLOCK_SPACING + BLOCK_TOP_PADDING;
+		foreach (TaskBlock t in blocks) {
+			float heightOffset = -(totalContentHeight + BLOCK_SPACING);
+			t.RecalculateBlock(heightOffset);
+			if (t.GetIsDragged()) {
+				SetShadow(t.GetComponent<RectTransform>().sizeDelta, heightOffset,
+					(t.GetNestingIndex() + 1f) * TaskBlock.NESTING_WIDTH);
+			}
+			totalContentHeight += t.GetHeight() + BLOCK_SPACING;
+		}
+		scrollViewContent.sizeDelta = new Vector2(scrollViewContent.sizeDelta.x, totalContentHeight);
+	}
+	//called every frame by a dragged block to determine where the block can go
+	//returns the first block [UNDER] the position of the mouse and the parent (default null)
+	public KeyValuePair<TaskBlock, TaskBlock> GetBlockAtLocation(Vector2 blockPosition) {
+		//TODO: find children blocks; if dragged to left of center screen, don't nest in children blocks
+
+		TaskBlock previousBlock = null;
+		foreach (TaskBlock b in blocks) {
+			if (b.GetIsDragged()) { continue; }
+			if (b.transform.position.y < blockPosition.y) {
+				//look through previous block's children
+				if (previousBlock != null) {
+					KeyValuePair<TaskBlock, TaskBlock> blocksToReturn = previousBlock.GetBlockAtPosition(blockPosition);
+					if (blocksToReturn.Key != null || blocksToReturn.Value != null) return blocksToReturn;
+				}
+				//no parent exists
+				return new(b, null);
+			}
+			previousBlock = b;
+		}
+		//edge case: last block
+		if (previousBlock != null) {
+			KeyValuePair<TaskBlock, TaskBlock> blocksToReturn = previousBlock.GetBlockAtPosition(blockPosition);
+			if (blocksToReturn.Key != null || blocksToReturn.Value != null) return blocksToReturn;
+		}
+		return new(null, null);
+	}
+	//rearranges blocks so that the movedBlock is now above the block titled aboveThis;
+	//if aboveThis is null movedBlock is the last block
+	public void RearrangeBlocks(TaskBlock movedBlock, TaskBlock aboveThis, TaskBlock parentBlock = null) {
+		//if a parent for this exists, make sure to remove it from that parent's list of children
+		movedBlock.DetachFromParent();
+
+		if (parentBlock != null) {
+			//Debug.Log($"set parent of movedBlock={movedBlock.GetBlockID()} to parentBlock={parentBlock.GetBlockID()}");
+			if (parentBlock.GetChildrenList().Contains(movedBlock))
+				parentBlock.GetChildrenList().Remove(movedBlock);
+
+			//push to end of parent's children
+			if (aboveThis == null) {
+				parentBlock.GetChildrenList().Add(movedBlock);
+			} else {
+				parentBlock.GetChildrenList().Insert(
+					parentBlock.GetChildrenList().IndexOf(aboveThis), movedBlock);
+			}
+			movedBlock.SetParent(parentBlock);
+		} else {
+			//Debug.Log($"Nest index: {movedBlock.GetNestingIndex()}");
+			if (blocks.Contains(movedBlock))
+				blocks.Remove(movedBlock);
+
+			//push to end
+			if (aboveThis == null) {
+				blocks.Add(movedBlock);
+			} else {
+				blocks.Insert(blocks.IndexOf(aboveThis), movedBlock);
+			}
+			movedBlock.SetParent(null);
+		}
+		movedBlock.RecalculateIndent();
+		foreach (TaskBlock b in movedBlock.GetChildrenList()) {
+			b.RecalculateIndent();
+		}
+		RecalculateBlocks();
+	}
+	public void DisableShadow() {
+		if (blockShadow.gameObject.activeInHierarchy)
+			blockShadow.gameObject.SetActive(false);
+	}
+	public void SetScrollViewDraggable(bool draggable) {
+		scrollViewParent.vertical = draggable;
+	}
+	private void Update() {
+	}
+	private void Awake() {
+		instance = this;
+	}
+}
---
+++ Assets/Scripts/TaskBlock.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class TaskBlock : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+	//default height of block when created
+	public const float DEFAULT_HEIGHT = 100f;
+	public const float NESTING_WIDTH = 50f;
+
+	#region Members
+
+	//changed when anything modifies block
+	private float height;
+	public float GetHeight() { return height; }
+
+	//how deep a block is nested
+	private int nestingIndex;
+	public int GetNestingIndex() { return nestingIndex; }
+
+	//when dragged, ignore position of this when re-sorting block indices
+	private bool isDragged = false;
+	public bool GetIsDragged() { return isDragged; }
+
+	//position offset between object position and mouse position
+	private Vector2 relativeDragPosition = Vector2.zero;
+
+	//TODO: block nesting
+	private List<TaskBlock> children = new();
+	public List<TaskBlock> GetChildrenList() { return children; }
+
+	private TaskBlock parent = null;
+	public TaskBlock GetParent() { return parent; }
+
+	//target indentation (ignore when dragging)
+	Vector2 targetOffsetMin = Vector2.zero, targetOffsetMax = Vector2.zero;
+	public Vector2 GetTargetOffsetMin() { return targetOffsetMin; }
+
+	#endregion
+
+	#region Dragging
+
+	public void OnPointerDown(PointerEventData eventData) {
+		isDragged = true;
+		relativeDragPosition = Input.mousePosition - transform.position;
+		GetComponent<RectTransform>().SetAsLastSibling();
+		BlockMaster.instance.SetScrollViewDraggable(false);
+	}
+	public void OnPointerUp(PointerEventData eventData) { }
+	private void UpdateDragging() {
+		transform.position = (Vector2)Input.mousePosition - relativeDragPosition;
+
+		//set this block right above blocks.Key and inside parent (blocks.Value)
+		KeyValuePair<TaskBlock, TaskBlock> blocks = BlockMaster.instance.GetBlockAtLocation(transform.position);
+		string k = blocks.Key != null ? blocks.Key.GetBlockID() : "none";
+		string v = blocks.Value != null ? blocks.Value.GetBlockID() : "none";
+		Debug.Log($"found block: {k}, {v}");
+		BlockMaster.instance.RearrangeBlocks(this, blocks.Key, blocks.Value);
+
+		//NOTE: for some reason, OnPointerUp doesn't work properly here
+		if (Input.GetMouseButtonUp(0)) {
+			isDragged = false;
+			BlockMaster.instance.SetScrollViewDraggable(true);
+			BlockMaster.instance.RecalculateBlocks();
+			BlockMaster.instance.DisableShadow();
+		}
+	}
+
+	#endregion
+
+	//recursively tries to get children blocks' positions (and checks if position's X
+	///  warrents a traversal into children depth) to find the block to return
+	public KeyValuePair<TaskBlock, TaskBlock> GetBlockAtPosition(Vector2 position) {
+		if (position.x < transform.position.x) return new(null, null);
+
+		//find first child with position y < position, then go to previous child and
+		//  recursively call GetBlockAtPosition on its children
+		TaskBlock previousBlock = null;
+		foreach (TaskBlock b in children) {
+			if (b.GetIsDragged()) continue;
+			if (b.transform.position.y < position.y) {
+				//look through previous block's children
+				if (previousBlock != null) {
+					KeyValuePair<TaskBlock, TaskBlock> blocksToReturn = previousBlock.GetBlockAtPosition(position);
+					if (blocksToReturn.Key != null || blocksToReturn.Value != null) return blocksToReturn;
+				}
+				//no child exists on previous
+				return new(b, this);
+			}
+			previousBlock = b;
+		}
+		//edge case: last block
+		if (previousBlock != null) {
+			KeyValuePair<TaskBlock, TaskBlock> blocksToReturn = previousBlock.GetBlockAtPosition(position);
+			if (blocksToReturn.Key != null || blocksToReturn.Value != null) return blocksToReturn;
+		}
+		return new(null, this);
+	}
+	public void DetachFromParent() {
+		if (parent != null) {
+			//remove self from parent
+			if (parent.GetChildrenList().Contains(this))
+				parent.GetChildrenList().Remove(this);
+		} else {
+			if (BlockMaster.instance.GetBlocks().Contains(this))
+				BlockMaster.instance.GetBlocks().Remove(this);
+		}
+	}
+	public void SetParent(TaskBlock parent) {
+		this.parent = parent;
+		RecalculateIndent();
+	}
+	public void RecalculateIndent() {
+		if (parent == null) {
+			nestingIndex = 0;
+		} else {
+			nestingIndex = parent.GetNestingIndex() + 1;
+		}
+
+		targetOffsetMin = new Vector2((nestingIndex + 1f) * NESTING_WIDTH, targetOffsetMin.y);
+
+		RectTransform rt = GetComponent<RectTransform>();
+
+		if (!isDragged) {
+			rt.offsetMin = new Vector2(targetOffsetMin.x, rt.offsetMin.y);
+			rt.offsetMax = new Vector2(targetOffsetMax.x, rt.offsetMax.y);
+		}
+	}
+	//TODO: temporary indexer for checking dragging, etc
+	public string GetBlockID() {
+		return transform.GetChild(0).GetComponent<TMP_Text>().text;
+	}
+	//TODO: temporary indexer for checking dragging, etc
+	public void SetBlockID(int id) {
+		transform.GetChild(0).GetComponent<TMP_Text>().text = $"Block {id}";
+	}
+	public void RecalculateBlock(float heightOffset) {
+		height = DEFAULT_HEIGHT;
+
+		foreach (TaskBlock b in children) {
+			//Debug.Log($"Nest index: {b.GetNestingIndex()}");
+			b.RecalculateBlock(heightOffset - height - BlockMaster.BLOCK_SPACING);
+			height += b.GetHeight() + BlockMaster.BLOCK_SPACING;
+		}
+		if (!isDragged) {
+			GetComponent<RectTransform>().anchoredPosition = new Vector2(
+				GetComponent<RectTransform>().anchoredPosition.x, heightOffset);
+		} else {
+			BlockMaster.instance.SetShadow(GetComponent<RectTransform>().sizeDelta, heightOffset,
+				(nestingIndex + 1f) * NESTING_WIDTH);
+		}
+
+		RecalculateIndent();
+	}
+	private void Awake() {
+		height = DEFAULT_HEIGHT;
+		nestingIndex = 0;
+		targetOffsetMin = GetComponent<RectTransform>().offsetMin;
+		targetOffsetMax = GetComponent<RectTransform>().offsetMax;
+	}
+	private void Update() {
+		if (isDragged) {
+			UpdateDragging();
+		}
+	}
+}
